9dfe659d0beee656c77cbc7331f78fbfe7c5821c
jahanashirin09 <jahanashrn411@gmail.com>
2024-12-09 09:01:35 +0530

    setup of home page header portion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
